d2853aefa4f7e13dbf15a2d8aab9bd4b8dd88184
rahulsharma0786 <77057152+rahulsharma0786@users.noreply.github.com>
2024-01-04 17:57:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express=require("express")
 const app=express()
 const mongoose=require("mongoose")
 const Student=require("./stucontrolar/stucon")
-const DB="mongodb+srv://rkumar45650:1234@cluster0.zqqxiua.mongodb.net/myelection?retryWrites=true&w=majority"
+const DB="mongodb+srv://rkumar45650:r1234@cluster0.z8ajs2p.mongodb.net/electionss?retryWrites=true&w=majority"
 mongoose.connect(DB).then(()=>{
     console.log("connect")
 })
